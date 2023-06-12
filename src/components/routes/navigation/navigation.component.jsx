87105cbf856d474d36d8d2daa1fe853deb35a2d5
Diego Auza <auza0002@algonquinlive.com>
2023-06-12 14:37:18 -0500

    profile photo added

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -3,12 +3,43 @@
 
 import { NavLink, Outlet } from "react-router-dom";
 import "./navigation.styles.scss";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import profileWEBP from "../../../assets/images/profile.webp";
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "rgba(234, 56, 41, 1)",
+    color: "rgba(234, 56, 41, 1)",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      border: "1px solid rgba(234, 56, 41, 1)",
+      content: '""',
+    },
+  },
+}));
+const profile = {
+  img: profileWEBP,
+  Name: "Diego Auza",
+  Age: "19",
+};
 const Navigation = () => {
   return (
     <section className="container-nav">
       <div className="second-container-nav">
         <div className="container-touchID">
-          <span className="material-symbols-outlined">fingerprint</span>
+          <span
+            style={{ userSelect: "none" }}
+            className="material-symbols-outlined"
+          >
+            fingerprint
+          </span>
         </div>
         <nav>
           <NavLink to="/" title="Link to go to the Dash Board page">
@@ -34,6 +65,19 @@ const Navigation = () => {
           </NavLink>
           <NavLink to="/settings" title="Link to go to the Settings page">
             <span className="material-symbols-outlined">settings</span>
+          </NavLink>
+          <NavLink
+            className="nav-profile"
+            to="/profile"
+            title="Link to go to the Profile page"
+          >
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar alt={`User avatar ${profile.Name}`} src={profile.img} />
+            </StyledBadge>
           </NavLink>
         </nav>
       </div>
