bacab83132d641959e82b26d8ade44287e9807af
Diego Auza <auza0002@algonquinlive.com>
2023-06-12 16:46:55 -0500

    Header 80% completed

--- src/components/routes/dashBoard/dashBoard.component.jsx
+++ src/components/routes/dashBoard/dashBoard.component.jsx
@@ -1,7 +1,28 @@
+/* eslint-disable no-unused-vars */
+import HeaderComponent from "../../header/header.component";
+
 const DashBoard = () => {
+  const links = {
+    1: "Partner-Account",
+    2: "network",
+  };
   return (
     <div>
-      <h2>DashBoard</h2>
+      <HeaderComponent links={links}>DashBoard</HeaderComponent>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis numquam
+        minus consequuntur maxime veritatis illum ipsa officia, explicabo, autem
+        eligendi unde earum magnam consequatur omnis quas. Veritatis nihil
+        impedit corporis aliquam odit architecto dolore officiis dignissimos
+        esse praesentium ipsa dolorum, qui, quos quam illum a sequi ad in
+        tenetur iusto explicabo ea voluptatum quis. Earum doloremque
+        necessitatibus fugiat? Expedita itaque commodi nisi, autem molestiae
+        quidem ad maxime ullam distinctio officiis ratione qui quam, quibusdam
+        enim fuga voluptatibus, consequuntur laudantium. Aspernatur, impedit
+        tempore. Eius esse nemo sequi eum sapiente quidem totam, labore quisquam
+        laboriosam iste architecto? Ipsam ipsa eos perspiciatis quis?
+      </p>
     </div>
   );
 };
--- src/style/style.jsx
+++ src/style/style.jsx
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 import styled from "styled-components";
 
-const title = styled.h1``;
+const MainContainer = styled.div`
+  margin-left: 8rem;
+  margin-right: 8rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+`;
 
-export { title };
+export { MainContainer };
