ae61e9601eed50e8bb75dcb7874f8f9016728454
Diego Auza <auza0002@algonquinlive.com>
2023-06-12 11:33:19 -0500

    google icon added

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // navigation
 
 import { Link, Outlet } from "react-router-dom";
@@ -5,11 +6,19 @@ const Navigation = () => {
   return (
     <section>
       <div>
-        <div></div>
+        <div>
+          <span className="material-symbols-outlined">fingerprint</span>
+        </div>
         <nav>
-          <Link to="/" title="Link to go to the Dash Board page"></Link>
-          <Link to="/client" title="Link to go to the Client page"></Link>
-          <Link to="/network" title="Link to go to the Network page"></Link>
+          <Link to="/" title="Link to go to the Dash Board page">
+            <span className="material-symbols-outlined">other_houses</span>
+          </Link>
+          <Link to="/client" title="Link to go to the Client page">
+            <span className="material-symbols-outlined">group</span>
+          </Link>
+          <Link to="/network" title="Link to go to the Network page">
+            <span className="material-symbols-outlined">schema</span>
+          </Link>
         </nav>
       </div>
     </section>
