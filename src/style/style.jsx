bc111e5fe27a76bb7d6cb95f7289cdc69bd58bc1
Diego Auza <auza0002@algonquinlive.com>
2023-06-13 10:49:04 -0500

    main responsive

--- src/style/style.jsx
+++ src/style/style.jsx
@@ -6,6 +6,11 @@ const MainContainer = styled.main`
   margin-right: 8rem;
   padding-top: 2rem;
   padding-bottom: 2rem;
+  transition: all 0.3s ease;
+  @media only screen and (max-width: 65em) {
+    margin-left: 4rem;
+    transition: all 0.5s ease;
+  }
 `;
 
 export { MainContainer };
